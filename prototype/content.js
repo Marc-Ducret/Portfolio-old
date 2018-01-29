41f38bfb6bc6a86cac056b2bec6e129ac0bc90d1
VengeurK <vengeurk@gmail.com>
2018-01-29 15:50:01 +0100

    dynamic content loading for projects list

--- prototype/content.js
+++ prototype/content.js
@@ -4,43 +4,73 @@ function generate_project_card(info) {
     elem.innerHTML =
         '<div class="card sticky-action hoverable">\n' +
         '    <div class="card-image waves-effect waves-block waves-light">\n' +
-        '        <img class="activator" src="'+info.img+'">\n' +
+        '        <img class="activator" src="' + info.img + '">\n' +
         '    </div>\n' +
         '    <div class="card-content">\n' +
-        '        <span class="card-title activator grey-text text-darken-4">'+info.name+'<i class="material-icons right">more_vert</i></span>\n' +
+        '        <span class="card-title activator grey-text text-darken-4">' + info.name + '<i class="material-icons right">more_vert</i></span>\n' +
         '    </div>\n' +
         '    <div class="card-reveal">\n' +
-        '        <span class="card-title grey-text text-darken-4">'+info.name+'<i class="material-icons right">close</i></span>\n' +
-        '        <p>'+info.details+'</p>\n' +
+        '        <span class="card-title grey-text text-darken-4">' + info.name + '<i class="material-icons right">close</i></span>\n' +
+        '        <p>' + info.details + '</p>\n' +
         '    </div>\n' +
         '\n' +
         '    <div class="card-action">\n' +
-        '        <a href="?content=project&project=do">Project Page</a>\n' +
+        '        <a href="?content=project&project=' + info.id + '">Project Page</a>\n' +
         '    </div>\n' +
         '</div>';
     return elem;
 }
 
-function generate_content() {
-    let content = [];
-    let content_type = new URL(document.location.href).searchParams.get('content');
-    switch(content_type) {
+function load_content() {
+    let content_node = document.getElementById('content');
+    let params = new URL(document.location.href).searchParams;
+    let content_type = params.get('content');
+    console.log('loading ' + content_type);
+    switch (content_type) {
         case 'projects':
-            for(let i = 0; i < 10; i++) {
-                content.push(generate_project_card({name: 'A project', img: 'card1.jpg', details: 'Some details.'}));
-            }
+            load_object('projects/list', function (list) {
+                let urls = [];
+                for (let l of list) {
+                    urls.push('projects/' + l);
+                }
+                load_objects(urls, function (projects) {
+                    for (let proj of projects) {
+                        content_node.appendChild(generate_project_card(proj));
+                    }
+                });
+            });
             break;
 
         case 'project':
-            let p = document.createElement('p');
-            p.innerText = 'hello';
-            content.push(p);
+            load_object('projects/' + params.get('project'), function (proj) {
+                let p = document.createElement('p');
+                p.innerText = JSON.stringify(proj);
+                content_node.appendChild(p);
+            });
             break;
     }
-
-    return content;
 }
 
-for(let c of generate_content()) {
-    document.getElementById('content').appendChild(c);
+function load_object(url, onload) {
+    let req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function () {
+        onload(JSON.parse(req.responseText));
+    };
+    req.send();
 }
+
+function load_objects(urls, onload) {
+    let ct = 0;
+    let objects = [];
+    for (let i in urls) {
+        load_object(urls[i], function (obj) {
+            objects[i] = obj;
+            if (++ct === urls.length) {
+                onload(objects);
+            }
+        });
+    }
+}
+
+load_content();
